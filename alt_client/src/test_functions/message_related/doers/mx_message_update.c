4ab7ba06df7cb117d06409d999bf34ebb502ca2c
Karyna Barinova <kbarinova@e3r3p1.unit.ua>
2020-12-01 14:21:34 +0200

    removed alt_client

--- alt_client/src/test_functions/message_related/doers/mx_message_update.c
+++
@@ -1,9 +0,0 @@
-//
-// Created by Rostyslav Druzhchenko on 26.11.2020.
-//
-
-#include <alt_client.h>
-
-void mx_message_update(char *token, t_message *message) {
-    mx_message_send_as_message(token, message);
-}
